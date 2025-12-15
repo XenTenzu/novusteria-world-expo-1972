0b71ae0059cba05a33c3f1c30c2a4f0c4873d9a9
XenTenzu <bianeza44@gmail.com>
2025-12-15 21:08:22 +0700

    Add Footer component with copyright information

---
+++ components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-deepNavy text-offWhite text-center py-8">
+      © 1972 Novusterria World Expo – Landerberk
+    </footer>
+  );
+}
